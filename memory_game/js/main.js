b32ac8f6963b6a8430c5754a8acdcd49bf7a735a
Dom Marrone <dom.marrone@gmail.com>
2019-06-06 18:40:27 -0400

    created game logic functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,30 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 /*
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
 */
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-//console.log("User flipped " + cardsInPlay);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay [1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
-	} } else {
-		alert("Not enough cards in play.")
+		console.log("Sorry, try again.");
+	} 
+}
+
+function flipCard(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
 	}
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
